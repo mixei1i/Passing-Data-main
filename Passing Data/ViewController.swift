ecf9df87fbc6e55af1065fc5650ceb5f4abe03b3
mixei1i <mixeilfuxashvili@yahoo.com>
2024-03-06 12:24:12 +0400

    Initial Commit

---
+++ Passing Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Passing Data
+//
+//  Created by Lasha Khizanishvili on 06.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
